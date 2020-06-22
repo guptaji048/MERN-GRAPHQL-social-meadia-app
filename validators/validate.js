1a467d2a0979f77acb0c0132cca4c170d437f36b
Rahul Gupta <guptaji048@gmail.com>
2020-06-22 14:08:44 +0530

    added validation for Create Post

--- validators/validate.js
+++ validators/validate.js
@@ -42,3 +42,18 @@ module.exports.validateLoginInput = (username, password) => {
     valid: Object.keys(errors).length < 1
   };
 };
+
+module.exports.validatePostInput = (title, photo_url) => {
+  const errors = {};
+  if (title.trim() === "") {
+    errors.title = "Post should have a title";
+  }
+  if (photo_url.trim() === "") {
+    errors.photo_url = "Post should have an Image";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1
+  };
+};
